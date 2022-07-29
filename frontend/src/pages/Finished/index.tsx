1232b8fe1026aee79542032d996471278cf64ccf
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-07-29 10:08:33 -0300

    Finished page estilização completa

---
+++ frontend/src/pages/Finished/index.tsx
@@ -0,0 +1,33 @@
+import { RegularText, TitleText } from "../../components/Typograhy";
+import { FinishedContainer, OrderDetailsContainer } from "./styles";
+import motoImg from '../../assets/moto.svg'
+import { InfoWithIcon } from "../../components/InfoWithIcon";
+import { MapPin, CurrencyDollar, Clock } from "phosphor-react";
+import { useTheme } from "styled-components";
+
+export function Finished() {
+  const { colors } = useTheme()
+  return (
+    <FinishedContainer className="container">
+      <div>
+        <TitleText size="l">Uhu! Pedido confirmado</TitleText>
+        <RegularText size="l">Agora é só aguardar que logo o café chegará até você</RegularText>
+      </div>
+
+      <section>
+        <OrderDetailsContainer>
+          <InfoWithIcon icon={<MapPin weight="fill" />} iconBg={colors["brand-purple"]}
+            text={<RegularText>Entrega em <strong>Rua João Daniel Martinelli, 102</strong> <br /> Farrapos - Porto Alegre, RS</RegularText>}
+          />
+          <InfoWithIcon icon={<Clock weight="fill" />} iconBg={colors["brand-yellow"]}
+            text={<RegularText>Previsão de entrega  <br /><strong>20 min - 30 min</strong></RegularText>}
+          />
+          <InfoWithIcon icon={<CurrencyDollar weight="fill" />} iconBg={colors["brand-yellow-dark"]}
+            text={<RegularText><strong>Cartão de crédito</strong></RegularText>}
+          />
+        </OrderDetailsContainer>
+        <img src={motoImg} alt="" />
+      </section>
+    </FinishedContainer>
+  )
+}
